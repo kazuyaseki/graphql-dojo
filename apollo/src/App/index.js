3181718665425bc10b82961af59cc562016fad1d
kazuya-seki <track613armory@gmail.com>
2018-05-27 22:08:45 +0900

    とりあえずレポジトリ情報を表示する

--- apollo/src/App/index.js
+++ apollo/src/App/index.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import Profile from '../Profile';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Profile />;
   }
 }
 
